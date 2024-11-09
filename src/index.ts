ce3630426e8f7901cd4b07abe0cb7771f6980b70
Frederik Metko <frederik.metko@gmail.com>
2024-11-09 12:29:51 +0100

    Handle 404

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import routes from './routes'
 import {dependencyInjector} from './middleware/dependencyInjector'
 import AppLogger from './utils/logger'
@@ -11,6 +11,10 @@ const app = express()
 app.use(dependencyInjector)
 
 app.use('/api', routes)
+
+app.use((req: Request, res: Response) => {
+    res.status(404).send('Not Found')
+})
 
 app.listen(config.server.port, () => {
     log.info(`Server started on port http://localhost:${config.server.port}`)
